b2fb7755a133ce8f895ea5cec9a55cf6d53fce98
aboode10 <100465041+aboode10@users.noreply.github.com>
2022-03-06 07:24:23 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import requests
+import flask
+from flask import *
+app = Flask("__name__")
+app.config["JSONIFY_PRETTYPRINT_REGULAR"]= True
+app.config["JSON_AS_ACSII"] = False
+@app.route("/")
+def q():
+ return jsonify(check=False, user=" ", Telegram="@JJJJzJJJ")
+@app.route("/check/telegram/")
+def sh():
+    user = request.args.get("User")
+    re = requests.get("https://t.me/{user}").text
+    if "tgme_username_link" in re:
+        return jsonify(Telegram="@JJJJzJJJ", re = User, User="True")
+    else:
+        return jsonify(Telegram="@JJJJzJJJ",              re = User, User="False")
+
+
+if __name__ == "__main__":
+	app.run()
